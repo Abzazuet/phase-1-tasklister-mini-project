4042592dece2fc9d815d4a1215fe28c4e529be80
Abel Zazueta <abel.zazueta@gmail.com>
2022-04-26 01:58:05 -0700

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  li.textContent = todo;
+  let btn = document.createElement('button');
+  let priority = document.getElementById('priority').value;
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'X';  
+  li.appendChild(btn);
+  li.style.backgroundColor = priority;  
+  document.querySelector('#tasks').appendChild(li);
+}
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
